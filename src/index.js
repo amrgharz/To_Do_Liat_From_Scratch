13c088ec0f0142bfba0a95eb87f647d6af4136b1
amrgharz <34239053+amrgharz@users.noreply.github.com>
2018-02-11 18:51:27 +0200

    I can catch the input

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 const styles = {
   fontFamily: 'sans-serif',
-  textAlign: 'center',
 };
 const Input = ({text , number , addlist})=>(
   <div>
@@ -11,11 +10,45 @@ const Input = ({text , number , addlist})=>(
   {text}
   <br/>
   </div>
-)
+);
+class Cont extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
 
+    };
+  }
+  onChange = (event)=>{
+  this.setState({term:event.target.value});
+  }
+  onSubmit = (event) =>{
+    event.preventDefault();
+    const newItems = [...this.state.items, this.state.term]
+    console.log(newItems)
+    this.setState({
+      term: '',
+      items: newItems
+
+    });
+  }
+  render(){
+    return(
+      <div>
+      <form className="Cont" onSubmit={this.onSubmit}>
+        <input value={this.state.term} onChange={this.onChange}/>
+        <button>Submit</button>
+      </form>
+      {this.state.items.map(item => <h4>{item}</h4>)}
+      </div>
+    );
+  }
+}
 const App = () => (
   <div style={styles}>
      <h2>My Basic To Do List</h2>
+     <Cont />
      <Input number="1" text="Create a github repo
      of my to do list"/>
      <Input number="2" text="make an ok button"/>
@@ -24,5 +57,6 @@ const App = () => (
      <Input number="4" text="make tasks chexkable"/>
   </div>
 );
+render(<App />, document.getElementById("root"));
 
-render(<App />, document.getElementById('root'));
+
